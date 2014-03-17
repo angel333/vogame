bb383e2069fff8b18544271688ae514e30c9ff16
Ondrej Simek <andrew@ondrejsimek.com>
2014-03-17 23:53:14 +0100

    Use full vendor libraries rather minified versions

--- gulpfile.js
+++ gulpfile.js
@@ -7,7 +7,16 @@ var gulp = require('gulp'),
 
 var libraries = [
 	'http://code.jquery.com/jquery-2.1.0.min.js',
-	'https://raw.github.com/less/less.js/master/dist/less-1.6.3.min.js'
+	'https://raw.github.com/less/less.js/master/dist/less-1.6.3.min.js',
+	'http://fb.me/react-with-addons-0.9.0.min.js',
+	'http://fb.me/JSXTransformer-0.9.0.js'
+];
+
+var devLibraries = [
+	'http://code.jquery.com/jquery-2.1.0.js',
+	'https://raw.github.com/less/less.js/master/dist/less-1.6.3.js',
+	'http://fb.me/react-with-addons-0.9.0.js',
+	'http://fb.me/JSXTransformer-0.9.0.js'
 ];
 
 
@@ -19,6 +28,12 @@ gulp.task('download-frontend-assets', function() {
 });
 
 
+gulp.task('download-frontend-dev-assets', function() {
+	return download(devLibraries)
+		.pipe(gulp.dest('public/scripts/vendor'));
+});
+
+
 gulp.task('clean', function() {
 	gulp.src('public/scripts/vendor.min.js', { read: false }).pipe(clean());
 	gulp.src('node_modules', { read: false }).pipe(clean());
@@ -26,4 +41,4 @@ gulp.task('clean', function() {
 });
 
 
-gulp.task('install', [ 'download-frontend-assets' ]);
+gulp.task('install', [ 'download-frontend-dev-assets' ]);
